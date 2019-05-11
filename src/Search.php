a2ee3f168c178edca466ac3b674ae1ede57f4cbe
kuzma <v.kuzma@mail.ru>
2019-05-11 23:31:58 +0300

    add validate

--- src/Search.php
+++ src/Search.php
@@ -4,10 +4,13 @@ namespace Grafline\Search;
 
 
 use App\Good;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class Search
 {
     protected $paginate;
+
+    use ValidatesRequests;
 
     function __construct($paginate=12)
     {
@@ -15,6 +18,9 @@ class Search
     }
 
     public function getSaearch($request){
+
+        $this->validate($request, ['search' => 'required|min:3']);
+
         $key = $request->input('search');
         $query = Good::choose()
             ->active()
