e6321089f162d8787846837e2494f7cff1edbc71
kuzma <v.kuzma@mail.ru>
2019-05-25 20:42:30 +0300

    edit

--- src/Search.php
+++ src/Search.php
@@ -3,18 +3,20 @@
 namespace Grafline\Search;
 
 
-use App\Good;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 class Search
 {
     protected $paginate;
+    protected $object;
 
     use ValidatesRequests;
 
-    function __construct($paginate=12)
+    function __construct($object, $paginate=12)
     {
         $this->paginate = $paginate;
+        $this->object = $object;
+        $this->object->localSearchable();
     }
 
     public function getSaearch($request){
@@ -22,8 +24,7 @@ class Search
         $this->validate($request, ['search' => 'required|min:3']);
 
         $key = $request->input('search');
-        $query = Good::choose()
-            ->active()
+        $query = $this->object->active()
             ->search($key);
             return $query->paginate($this->paginate);
     }
